eeabe8c88e0eda77096eec9144c3321cad084ece
TheHuessy <43474783+TheHuessy@users.noreply.github.com>
2019-03-11 11:38:56 -0400

    Update Craigslist_Scraper_1.py
    battery dying, this is not functioning as is, go back into line 446

--- Craigslist_Scraper_1.py
+++ Craigslist_Scraper_1.py
@@ -440,8 +440,12 @@ for z in range(len(zips)):
     
     ## Check the clpull most recent post date and add it to ctime
     mrp = max(clpull['Post_Date'])
-    mrp = mrp.replace("-0500", "")
-    mrp = mrp.replace("T", " ")
+    #mrp = mrp.replace("-0500", "")
+    #mrp = mrp.replace("T", " ")
+    mrp = re.sub('\..*', '', mrp)
+    #Test what happens when you do the above regex with a minus sign instead of a period
+    #tm = re.sub('T', ' ', tm)
+    
     mb = datetime.datetime.strptime(mrp, "%Y-%m-%d %H:%M:%S")
     
     ctime.loc[ctime.zips == int(zips[z]), 'mrd'] = mb
